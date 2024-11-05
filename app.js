7c17846ecc900c3278382c3e3c06e55136d77c30
KaZaKDon <strannik19742@mail.ru>
2024-11-05 22:42:20 +0300

    Основные типы TS

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+let revenue = 1000;
+let bonus = 500;
+let res = revenue + bonus;
+console.log(res);
+
+
+function getFullName(firstname, surmame) {
+    return `${firstname}  ${surmame}`;
+}
+const getFullName1 = (firstname, surmame) => {
+    return `${firstname}  ${surmame}`;
+};
---
+++ app.ts
@@ -0,0 +1,13 @@
+let revenue = 1000
+let bonus = 500
+
+let res = revenue + bonus
+console.log(res)
+
+
+function getFullName(firstname: string, surmame: string): string {
+    return `${firstname}  ${surmame}`
+}
+const getFullName1 = (firstname: string, surmame: string): string => {
+    return `${firstname}  ${surmame}`
+}
